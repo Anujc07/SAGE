befdc9c2554c09908b4c1cd783dccbbb9e64b013
Anuj Choubey <114551422+Anujc07@users.noreply.github.com>
2024-07-23 14:29:57 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,14 +2,18 @@
 
 echo "BUILD START"
 
+# Update and install MySQL client libraries
+apt-get update && apt-get install -y default-libmysqlclient-dev
+
 # Set MYSQLCLIENT environment variables
 export MYSQLCLIENT_CFLAGS=$(mysql_config --cflags)
 export MYSQLCLIENT_LDFLAGS=$(mysql_config --libs)
 
-# Install dependencies
+# Install Python dependencies
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
-# Create the output directory
+# Create the output directory for static files
 mkdir -p staticfiles_build/static
 
 # Collect static files
