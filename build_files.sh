9047a631a26877ed278ab40e5199a46aee78586f
Anuj Choubey <114551422+Anujc07@users.noreply.github.com>
2024-07-23 14:21:46 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
+#!/bin/bash
+
 echo "BUILD START"
+
+# Install dependencies
 python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+
+# Create the output directory
+mkdir -p staticfiles_build/static
+
+# Collect static files
+python3.9 Highrise/manage.py collectstatic --noinput --clear --settings=Highrise.settings
+
 echo "BUILD END"
